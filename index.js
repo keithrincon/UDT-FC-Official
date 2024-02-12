21bca377d002779d5850d5e624710f7d80066354
Keith Rincon <keithrt3008@gmail.com>
2024-02-12 14:16:24 -0800

    Site pages completed

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ navBtnDOM.addEventListener('click', () => {
 const date = getElement('#date');
 const currentYear = new Date().getFullYear();
 date.textContent = currentYear;
+
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+  });
+}
+
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName('city');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  document.getElementById(cityName).style.display = 'block';
+}
